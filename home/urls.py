159db16f135d690792a9de6131be69f601c16eb8
Andre Vinicius <avgc2411@gmail.com>
2024-12-13 20:38:57 -0300

    CRUD

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name="home"),
     path('categoria/lista/',views.categoria, name="categoria"),
     path('categoria/formulario/',views.form_categoria, name="formulario"),
-    
+    path('categoria/formulario/<int:id>/', views.editar_categoria, name='editar_categoria'),
+    path('categoria/detalhes/<int:id>/', views.detalhes_categoria, name='detalhes_categoria'),
+    path('categoria/remover/<int:id>/', views.remover_categoria, name='remover_categoria'),  # URL para excluir categoria    
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from .forms import *
 
@@ -24,3 +26,31 @@ def form_categoria(request):
         'form':form,
     }
     return render(request, 'categoria/formulario.html', contexto)
+
+def editar_categoria(request, id):
+    categoria = Categoria.objects.get(pk=id)
+    if request.method == 'POST':
+        # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            categoria = form.save() # save retorna o objeto salvo
+            return redirect('categoria') # redireciona para a listagem
+    else:
+         form = CategoriaForm(instance=categoria)
+    return render(request, 'categoria/formulario.html', {'form': form, 'is_edit': True})
+
+def remover_categoria(request, id):
+    if request.method == "DELETE":
+        try:
+            # Obtém o objeto Categoria ou retorna 404 se não encontrado
+            categoria = get_object_or_404(Categoria, pk=id)
+            categoria.delete()  # Exclui a categoria
+            return JsonResponse({'success': True}, status=200)  # Retorna resposta de sucesso
+        except Exception as e:
+            # Retorna erro em caso de falha
+            return JsonResponse({'success': False, 'error': str(e)}, status=500)
+    return JsonResponse({'success': False}, status=400)  # Caso não seja uma requisição DELETE
+
+def detalhes_categoria(request, id):
+    categoria = get_object_or_404(Categoria, pk=id)
+    return render(request, 'categoria/detalhes.html', {'categoria': categoria})
