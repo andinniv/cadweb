69b49fc3446de1591a50da065bb730e95788b5bf
Andre Vinicius <avgc2411@gmail.com>
2024-12-17 20:28:23 -0300

    messages

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from .forms import *
 
@@ -28,28 +27,38 @@ def form_categoria(request):
     return render(request, 'categoria/formulario.html', contexto)
 
 def editar_categoria(request, id):
-    categoria = Categoria.objects.get(pk=id)
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado!')
+        return redirect('categoria')  # Redireciona para a listagem
+
+
     if request.method == 'POST':
         # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
         form = CategoriaForm(request.POST, instance=categoria)
         if form.is_valid():
             categoria = form.save() # save retorna o objeto salvo
-            return redirect('categoria') # redireciona para a listagem
+            messages.success(request, 'Operação realizada com Sucesso!')
+            lista = []
+            lista.append(categoria) 
+            return render(request, 'categoria/lista.html', {'lista': lista})
     else:
          form = CategoriaForm(instance=categoria)
     return render(request, 'categoria/formulario.html', {'form': form, 'is_edit': True})
 
 def remover_categoria(request, id):
-    if request.method == "DELETE":
-        try:
-            # Obtém o objeto Categoria ou retorna 404 se não encontrado
-            categoria = get_object_or_404(Categoria, pk=id)
-            categoria.delete()  # Exclui a categoria
-            return JsonResponse({'success': True}, status=200)  # Retorna resposta de sucesso
-        except Exception as e:
-            # Retorna erro em caso de falha
-            return JsonResponse({'success': False, 'error': str(e)}, status=500)
-    return JsonResponse({'success': False}, status=400)  # Caso não seja uma requisição DELETE
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        messages.error(request, 'Registro não encontrado!')
+        return redirect('categoria')  # Redireciona para a listagem
+
+    if request.method == "POST":
+        categoria.delete()
+        messages.success(request, 'Operação realizada com Sucesso!')
+        return redirect('categoria')
 
 def detalhes_categoria(request, id):
     categoria = get_object_or_404(Categoria, pk=id)
