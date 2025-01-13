95f5b01892ea0513f0cf59e4cf2b50c05e9e1f36
Andre Vinicius <avgc2411@gmail.com>
2025-01-13 18:59:24 -0300

    btns

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('categoria/formulario/',views.form_categoria, name="formulario"),
     path('categoria/formulario/<int:id>/', views.editar_categoria, name='editar_categoria'),
     path('categoria/detalhes/<int:id>/', views.detalhes_categoria, name='detalhes_categoria'),
-    path('categoria/remover/<int:id>/', views.remover_categoria, name='remover_categoria'),  # URL para excluir categoria    
+    path('categoria/remover/<int:id>/', views.remover_categoria, name='remover_categoria'),
+
+    path('cliente/', views.cliente, name="cliente"),
+    path('cliente/form_cliente/', views.form_cliente, name="form_cliente"),
+    path('cliente/detalhes/<int:id>/', views.detalhes_cliente, name="detalhes_cliente"),
+    path('editar_cliente/<int:id>/', views.editar_cliente, name="editar_cliente"),
+    path('cliente/excluir/<int:id>/', views.excluir_cliente, name='excluir_cliente'),    
 ]
